506a3d5357065ab5aa65488c099377400d71ac84
Matheus Gobetti <matheusgobetti12@gmail.com>
2021-03-14 23:06:47 -0300

    Javascript file

--- todo-list/assets/js/main.js
+++ todo-list/assets/js/main.js
@@ -1,47 +1,75 @@
-// Cronômetro em Javascript
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
 
-function relogio() {
-  function criaHoraDosSegundos(segundos) {
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-      hour12: false,
-      timeZone: 'GMT'
-    });
-  }
-  
-  const relogio = document.querySelector('.relogio');
-  let segundos = 0;
-  let timer;
-  
-  function iniciaRelogio() {
-    timer = setInterval(function() {
-      segundos++;
-      relogio.innerHTML = criaHoraDosSegundos(segundos);
-    }, 1000);
-  }
-  
-  document.addEventListener('click',function(e) {
-    const elemento = e.target;
-  
-    if(elemento.classList.contains('zerar')) {
-      clearInterval(timer);
-      relogio.innerHTML = '00:00:00';
-      relogio.classList.remove('pausado');
-      segundos = 0;
-    }
-  
-    if(elemento.classList.contains('iniciar')) {
-      relogio.classList.remove('pausado');
-      clearInterval(timer);
-      iniciaRelogio();
-    }
-  
-    if(elemento.classList.contains('pausar')) {
-      clearInterval(timer);
-      relogio.classList.add('pausado');
-    }
-  });
+function criaLi() {
+  const li = document.createElement('li');
+  return li;
 }
 
-relogio();
+inputTarefa.addEventListener('keypress', function(e) {
+  if(e.keyCode === 13) {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+  }
+});
 
+function limpaInput() {
+  inputTarefa.value = '';
+  inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+  li.innerText += ' ';
+  const botaoApagar = document.createElement('button');
+  botaoApagar.innerText = 'Apagar';
+  botaoApagar.setAttribute('class', 'apagar');
+  botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+  li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+  const li = criaLi();
+  li.innerText = textoInput;
+  tarefas.appendChild(li);
+  limpaInput();
+  criaBotaoApagar(li);
+  salvarTarefas();
+}
+
+btnTarefa.addEventListener('click', function() {
+  if (!inputTarefa.value) return;
+  criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function(e) {
+  const elemento = e.target;
+
+  if(elemento.classList.contains('apagar')) {
+    elemento.parentElement.remove();
+    salvarTarefas();
+  }
+})
+
+function salvarTarefas() {
+  const liTarefas = tarefas.querySelectorAll('li');
+  const listaDeTarefas = [];
+
+  for(let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+    listaDeTarefas.push(tarefaTexto);
+  }
+  const tarefasJSON = JSON.stringify(listaDeTarefas);
+  localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+  const tarefas = localStorage.getItem('tarefas');
+  const listaDeTarefas = JSON.parse(tarefas);
+  for(let tarefa of listaDeTarefas) {
+    criaTarefa(tarefa);
+  }
+}
+
+adicionaTarefasSalvas(); 
