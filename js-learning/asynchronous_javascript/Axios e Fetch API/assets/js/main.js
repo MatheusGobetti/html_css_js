a8b072db3028bd5dc8512495e0155cb60255d6f7
Matheus Gobetti <matheusgobetti12@gmail.com>
2021-08-26 19:05:16 -0300

    Utilizando AXIOS ou FETCH API para exibir dados em json numa pagina HTML

---
+++ js-learning/asynchronous_javascript/Axios e Fetch API/assets/js/main.js
@@ -0,0 +1,35 @@
+// Usando FETCH API (utiliza fetch):
+
+// fetch('pessoas.json')
+//   .then(resposta => resposta.json())
+//   .then(json => carregaElementosNaPagina(json));
+
+// Usando AXIOS (utiliza XMLHTTPRequest):
+
+axios('pessoas.json')
+  .then(resposta => carregaElementosNaPagina(resposta.data));
+
+// Função para carregar elementos na pagina:
+
+function carregaElementosNaPagina(json) {
+  const table = document.createElement('table');
+  for(let pessoa of json) {
+    const tr = document.createElement('tr');
+
+    let td1 = document.createElement('td');
+    td1.innerHTML = pessoa.nome;
+    tr.appendChild(td1);
+
+    let td2 = document.createElement('td');
+    td2.innerHTML = pessoa.idade;
+    tr.appendChild(td2);
+
+    let td3 = document.createElement('td');
+    td3.innerHTML = pessoa.salario;
+    tr.appendChild(td3);
+
+    table.appendChild(tr);
+  }
+  const resultado = document.querySelector('.resultado');
+  resultado.appendChild(table);
+}
